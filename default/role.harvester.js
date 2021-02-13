3f0948ba3316a121257f80c5f3236536334cd210
Louis <lpaube@hotmail.com>
2021-02-13 00:30:49 -0400

    update

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -7,8 +7,16 @@ var roleHarvester = {
                 creep.moveTo(sources[0]);
             }
         } else {
-            if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            console.log('test');
+            let structs = creep.room.find(FIND_STRUCTURES);
+            let targets = _.filter(structs, function(struct) {
+                return (struct.structureType == STRUCTURE_SPAWN || struct.structureType == STRUCTURE_EXTENSION) && struct.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            });
+            console.log(targets.length);
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
             }
         }
     }
